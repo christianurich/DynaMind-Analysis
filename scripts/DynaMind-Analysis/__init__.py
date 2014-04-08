3560d320f730ced67007f75d9fcdf903e8210bcd
Christian Urich <christian.urich@gmail.com>
2014-04-08 13:55:04 +1000

    add initial three js plugin

---
+++ scripts/DynaMind-Analysis/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ DAnCEKit
+
+                              A web interface to bring DynaMind online
+                              -------------------
+        begin                : 2014-04-01
+        copyright            : (C) 2014 Christian Urich
+        email                : christian.urich@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+from viewtothreejs import *
